c59ebe11ee9c3118bf130632272d5e6ab4a7789c
rechar <884520248@qq.com>
2021-07-22 15:34:27 +0800

    webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,6 +9,24 @@ using namespace std;
 void get_URL(const string &host, const string &path) {
     // Your code here.
 
+    TCPSocket _socket = TCPSocket();
+    Address address = Address(host, "http");
+
+    _socket.connect(address);
+
+    _socket.write("GET " + path +" HTTP/1.1" + "\r\n" + "Host: " + host + "\r\n\r\n");
+    
+    _socket.shutdown(SHUT_WR);
+
+
+    string rec = _socket.read();
+
+    cout << rec;
+
+    
+
+    
+
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -17,8 +35,8 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
